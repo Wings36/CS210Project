3f80c8884d701f65f9718847c93ce9c87b8bfbca
BuildTools <unconfigured@null.spigotmc.org>
2019-11-25 20:17:36 -0800

    test

--- src/Employee.java
+++ src/Employee.java
@@ -1,6 +1,9 @@
 public class Employee {
 	private String prompt = "Peter bad";
 	private String bad = "Dien";
-
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world");
+	}
 
 }
