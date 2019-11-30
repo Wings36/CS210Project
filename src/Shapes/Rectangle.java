02c76aa2eaf0133d718b9658762b11743cf52311
Peter Hoang <peterhg7@gmail.com>
2019-11-30 15:39:26 -0800

    Created the Rectangle Class Implemented the shape interface Added methods to set the length and height of the Rectangle

--- src/Shapes/Rectangle.java
+++ src/Shapes/Rectangle.java
@@ -1,4 +1,33 @@
 package Shapes;
+/*
+            Side B
+        ----------------
+ Side A |              |
+        |              |
+        ----------------
+*/
+public class Rectangle implements Shapes {
 
-public class Rectangle {
+    private int sideA;
+    private int sideB;
+
+    public int area() {
+        return sideA * sideB;
+    }
+
+    public int perimeter() {
+        return (sideB * 2) + (sideA * 2);
+    }
+
+    public void setLength(int length)
+    {
+        sideB = length;
+        System.out.println("Successfully set the length to " + sideB);
+    }
+
+    public void setHeight(int height)
+    {
+        sideA = height;
+        System.out.println("Successfully set the height to " + sideA);
+    }
 }
