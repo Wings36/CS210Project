d05f13b0f63aee9ad1cea28c0d987aadee4c10c7
Anais <Anais@Anais>
2019-11-30 18:48:30 -0800

    This is the interface for Employee. From this, you create new employees, such as Lawyer, Janitor, Scientist Employee classes.

--- src/Employee.java
+++ src/Employee.java
@@ -1,8 +1,8 @@
-public class Employee {
-	
-	public static void main(String[] args)
-	{
-		System.out.println("Hello world");
-	}
-
+public interface Employee {
+	public int getSalary();
+	public String getJobDescription();
+	public int getWeeklyHours();
+	public void addHoursWorked();
+	public void addHoursWorked(int hours);
+	public int getHoursWorked();
 }
