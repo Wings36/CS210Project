e97f348edfd2bb65bfeeee4c22dfa7600a332c85
BuildTools <unconfigured@null.spigotmc.org>
2019-12-01 16:16:40 -0800

    Fixed Secretary

--- src/Secretary.java
+++ src/Secretary.java
@@ -1,28 +1,2 @@
 //Created by Peter Hoang
-public class Secretary implements Employee {
-
-    private int salary;
-    private int scheduledHours;
-    private int workedHours;
-
-    public Secretary()
-    {
-        salary = 43507;
-        scheduledHours = 30;
-        workedHours = 0;
-    }
-
-    public int getSalary() { return salary; }
-    public int getWeeklyHours() { return scheduledHours; }
-    public int getHoursWorked() { return workedHours; }
-    public void addHoursWorked(int hours) { workedHours += hours; }
-    public void removeHoursWorked(int hours) {
-        if (workedHours - hours < 0) { throw new IllegalArgumentException("Hours worked cannot be negative"); }
-        workedHours -= hours;
-    }
-    public String getJobDescription() {
-        return "a person employed by an individual or in an office to assist with correspondence, keep records, make appointments, and carry out similar tasks.";
-    }
-
-
-}
+public class Secretary implements Employee { private int salary; private int scheduledHours; private int workedHours; public Secretary() { salary = 43507; scheduledHours = 30; workedHours = 0; } public int getSalary() { return salary; } public int getWeeklyHours() { return scheduledHours; } public int getHoursWorked() { return workedHours; } public void addHoursWorked(int hours) { workedHours += hours; } public void removeHoursWorked(int hours) { if (workedHours - hours < 0) { throw new IllegalArgumentException("Hours worked cannot be negative"); } workedHours -= hours; } public String getJobDescription() { return "a person employed by an individual or in an office to assist with correspondence, keep records, make appointments, and carry out similar tasks."; } }
