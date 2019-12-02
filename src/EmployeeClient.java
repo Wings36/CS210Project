dbed5b1cb8bcb33235d6c148029ef86fc59173a3
BuildTools <unconfigured@null.spigotmc.org>
2019-12-01 21:42:37 -0800

    Messing around with gradle

--- src/EmployeeClient.java
+++ src/EmployeeClient.java
@@ -20,7 +20,7 @@ public class EmployeeClient {
 	
 		tony.addHoursWorked(35);
 		tony.addHoursWorkedHome(6);
-		joe.addHoursWorked(50);1
+		joe.addHoursWorked(50);
 		ana.addHoursWorked(44);
 		
 		System.out.println("\nTimesheet for employees this week: ");
